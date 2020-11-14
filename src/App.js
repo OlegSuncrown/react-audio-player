199f149447a45e438f704fa13bdfb038e737d83b
Oleg Biblyi <oleg_biblyi@hotmail.com>
2020-11-14 17:19:36 +0200

    bind slider to audio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
+import { useState, useRef } from 'react';
 import Slider from './components/slider/Slider';
+import song from './Suncrown - Legend of the Forgotten Centuries.mp3'
 
 function App() {
+  const [percentage, setPercentage] = useState(0);
+  const audioRef = useRef();
+
+  const getCurrDuration = (e) => {
+    const percent = ((e.currentTarget.currentTime  / e.currentTarget.duration) * 100).toFixed(0);
+    setPercentage(+percent);
+  }
+
+  const play = () => {
+    const audio = audioRef.current;
+    audio.volume = 0.1
+    audio.play();
+  }
+ 
+  const onChange = (e) => {
+    setPercentage(e.target.value)
+    const audio = audioRef.current;
+    audio.currentTime = audio.duration/100 * e.target.value
+  };
+
   return (
     <div className="app-container">
       <h1>Player</h1>
-      <Slider />
+      <Slider 
+        percentage={percentage} 
+        setPercentage={setPercentage}
+        onChange = {onChange}
+      />
+      <audio ref={audioRef} onTimeUpdate={(e) => {
+        getCurrDuration(e)
+     
+      }} 
+        src={song}>
+      
+      </audio>
+      <button onClick={() => play()}>Play</button>
     </div>
   );
 }
--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,25 +1,27 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import './slider.css';
 import './thumb.css';
 
-function Slider() {
+function Slider({percentage = 0, onChange}) {
   const [position, setPosition] = useState(0);
   const [marginLeft, setMarginLeft] = useState('0');
   const [paddingRight, setPaddingRight] = useState('0');
 
   const thumbRef = useRef();
 
-  const onChange = (e) => {
+  useEffect(() => {
+    let val = percentage
+  
     const thumbWidth = thumbRef.current.getBoundingClientRect().width;
 
-    const centerThumb = (thumbWidth / 100) * e.target.value * -1;
+    const centerThumb = (thumbWidth / 100) * val  * -1;
 
-    const centerProgressBar = thumbWidth - (thumbWidth / 100) * e.target.value;
+    const centerProgressBar = thumbWidth - (thumbWidth / 100) * val;
 
-    setPosition(+e.target.value);
+    setPosition(+val);
     setMarginLeft(centerThumb);
     setPaddingRight(centerProgressBar);
-  };
+  }, [percentage])
 
   return (
     <div className='slider-container'>
@@ -32,7 +34,8 @@ function Slider() {
         className='thumb'
         style={{ left: `${position}%`, marginLeft: `${marginLeft}px` }}
       ></div>
-      <input type='range' className='range' onChange={onChange} />
+      {/* <input type='range'  value={percentage} /> */}
+      <input type='range' className='range' value={position} onChange={onChange} />
     </div>
   );
 }
