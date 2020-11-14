eb1825e130cdff9b99a0c5d3dcee0f8dd82d3f06
Oleg Biblyi <oleg_biblyi@hotmail.com>
2020-11-14 17:27:40 +0200

    make smaller slider steps

--- src/App.js
+++ src/App.js
@@ -1,46 +1,42 @@
-import { useState, useRef } from 'react';
-import Slider from './components/slider/Slider';
+import { useState, useRef } from 'react'
+import Slider from './components/slider/Slider'
 import song from './Suncrown - Legend of the Forgotten Centuries.mp3'
 
 function App() {
-  const [percentage, setPercentage] = useState(0);
-  const audioRef = useRef();
+  const [percentage, setPercentage] = useState(0)
+  const audioRef = useRef()
 
   const getCurrDuration = (e) => {
-    const percent = ((e.currentTarget.currentTime  / e.currentTarget.duration) * 100).toFixed(0);
-    setPercentage(+percent);
+    const percent = ((e.currentTarget.currentTime / e.currentTarget.duration) * 100).toFixed(2)
+    setPercentage(+percent)
   }
 
   const play = () => {
-    const audio = audioRef.current;
+    const audio = audioRef.current
     audio.volume = 0.1
-    audio.play();
+    audio.play()
   }
- 
+
   const onChange = (e) => {
     setPercentage(e.target.value)
-    const audio = audioRef.current;
-    audio.currentTime = audio.duration/100 * e.target.value
-  };
+    const audio = audioRef.current
+    audio.currentTime = (audio.duration / 100) * e.target.value
+  }
 
   return (
-    <div className="app-container">
+    <div className='app-container'>
       <h1>Player</h1>
-      <Slider 
-        percentage={percentage} 
-        setPercentage={setPercentage}
-        onChange = {onChange}
-      />
-      <audio ref={audioRef} onTimeUpdate={(e) => {
-        getCurrDuration(e)
-     
-      }} 
-        src={song}>
-      
-      </audio>
+      <Slider percentage={percentage} onChange={onChange} />
+      <audio
+        ref={audioRef}
+        onTimeUpdate={(e) => {
+          getCurrDuration(e)
+        }}
+        src={song}
+      ></audio>
       <button onClick={() => play()}>Play</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,26 +1,26 @@
-import { useState, useRef, useEffect } from 'react';
-import './slider.css';
-import './thumb.css';
+import { useState, useRef, useEffect } from 'react'
+import './slider.css'
+import './thumb.css'
 
-function Slider({percentage = 0, onChange}) {
-  const [position, setPosition] = useState(0);
-  const [marginLeft, setMarginLeft] = useState('0');
-  const [paddingRight, setPaddingRight] = useState('0');
+function Slider({ percentage = 0, onChange }) {
+  const [position, setPosition] = useState(0)
+  const [marginLeft, setMarginLeft] = useState('0')
+  const [paddingRight, setPaddingRight] = useState('0')
 
-  const thumbRef = useRef();
+  const thumbRef = useRef()
 
   useEffect(() => {
     let val = percentage
-  
-    const thumbWidth = thumbRef.current.getBoundingClientRect().width;
 
-    const centerThumb = (thumbWidth / 100) * val  * -1;
+    const thumbWidth = thumbRef.current.getBoundingClientRect().width
 
-    const centerProgressBar = thumbWidth - (thumbWidth / 100) * val;
+    const centerThumb = (thumbWidth / 100) * val * -1
 
-    setPosition(+val);
-    setMarginLeft(centerThumb);
-    setPaddingRight(centerProgressBar);
+    const centerProgressBar = thumbWidth - (thumbWidth / 100) * val
+
+    setPosition(+val)
+    setMarginLeft(centerThumb)
+    setPaddingRight(centerProgressBar)
   }, [percentage])
 
   return (
@@ -37,7 +37,7 @@ function Slider({percentage = 0, onChange}) {
       {/* <input type='range'  value={percentage} /> */}
       <input type='range' className='range' value={position} onChange={onChange} />
     </div>
-  );
+  )
 }
 
-export default Slider;
+export default Slider
