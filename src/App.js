ca85d6da8418cb8faf181b3b8147e45da6799f3c
Oleg Biblyi <oleg_biblyi@hotmail.com>
2020-11-21 18:27:48 +0200

    added timer

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ function App() {
   const [percentage, setPercentage] = useState(0)
   const [isPlaying, setIsPlaying] = useState(false)
   const [duration, setDuration] = useState(0)
-  const [currentTime, setCurrentTime] = useState('0.00')
+  const [currentTime, setCurrentTime] = useState(0)
 
   const audioRef = useRef()
 
   const getCurrDuration = (e) => {
     const percent = ((e.currentTarget.currentTime / e.currentTarget.duration) * 100).toFixed(2)
-    const time = e.currentTarget.currentTime / 60
+    const time = e.currentTarget.currentTime /// 60
 
     setPercentage(+percent)
     setCurrentTime(time.toFixed(2))
@@ -49,7 +49,7 @@ function App() {
         ref={audioRef}
         onTimeUpdate={getCurrDuration}
         onLoadedData={(e) => {
-          setDuration((e.currentTarget.duration / 60).toFixed(2))
+          setDuration(e.currentTarget.duration.toFixed(2))
         }}
         src={song}
       ></audio>
--- src/components/controls/ControlPanel.js
+++ src/components/controls/ControlPanel.js
@@ -5,11 +5,39 @@ import './control-panel.css'
 function ControlPanel({ play, isPlaying, duration, currentTime }) {
   //const audio = audioRef.current
   //console.log(audioRef.current.currentTime)
+  function secondsToHms(seconds) {
+    if (!seconds) return '00m 00s'
+
+    let duration = seconds
+    let hours = duration / 3600
+    duration = duration % 3600
+
+    let min = parseInt(duration / 60)
+    duration = duration % 60
+
+    let sec = parseInt(duration)
+
+    if (sec < 10) {
+      sec = `0${sec}`
+    }
+    if (min < 10) {
+      min = `0${min}`
+    }
+
+    if (parseInt(hours, 10) > 0) {
+      return `${parseInt(hours, 10)}h ${min}m ${sec}s`
+    } else if (min == 0) {
+      return `00m ${sec}s`
+    } else {
+      return `${min}m ${sec}s`
+    }
+  }
+
   return (
     <div className='control-panel'>
-      <div className='timer'>{currentTime}</div>
+      <div className='timer'>{secondsToHms(currentTime)}</div>
       <Button play={play} isPlaying={isPlaying} />
-      <div className='timer'>{duration}</div>
+      <div className='timer'>{secondsToHms(duration)}</div>
     </div>
   )
 }
